606b96bc3237477646c624e88701bb52ada29b36
Oktay Gardener <oktayb@kth.se>
2016-02-28 22:01:53 -0800

    cont. work on UI, logic, transitions between views

--- CPEGradeBookCalPoly/SignInViewController.swift
+++ CPEGradeBookCalPoly/SignInViewController.swift
@@ -2,13 +2,27 @@
 //  SignInViewController.swift
 //  CPEGradeBookCalPoly
 //
-//  Created by Oktay Gardener on 23/02/16.
+//  Created by Oktay Gardener on 28/02/16.
 //  Copyright © 2016 Oktay Gardener. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-class SignInViewController: ViewController {
+class SignInViewController: UIViewController {
+    @IBOutlet var loginButton: UIButton!
+    
+    @IBAction func loginTapped() {
+        self.performSegueWithIdentifier("LoginSuccessful", sender: self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -16,4 +30,15 @@ class SignInViewController: ViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewDidLoad()
     }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
